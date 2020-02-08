033b22cbc2975d9e9f1f9950ef653958b4a7acf5
thiago orlandini <thiagorlandini@hotmail.com>
2020-02-08 15:57:31 -0300

    fisrt

---
+++ src/main/java/com/jb421/Jb421Application.java
@@ -0,0 +1,13 @@
+package com.jb421;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jb421Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jb421Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/jb421/Jb421ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jb421;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jb421ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
